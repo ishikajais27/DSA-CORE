b600a87ccfbe413bff5cab10d6b31815ba8a2b5c
ishikajais27 <ishikajais09876@gmail.com>
2026-02-19 12:03:55 +0530

    Solved E 27q

---
+++ Array/Easy/RemoveElement.java
@@ -0,0 +1,33 @@
+public class RemoveElement{
+    public static void main(String[] args) {
+        int[] nums = {0,1,2,2,3,0,4,2};
+        int val = 2;
+        System.out.println(removeElement(nums,val));
+    }
+   static int removeElement(int[] nums, int val) {
+        int count = nums.length;   
+        int k=0;
+        
+        for (int i = nums.length - 1; i >= 0; i--) {
+           // APROACH 1
+            // if (nums[i] == val) {
+            //     count--;          
+                
+            //     int j = i;
+            //     while (j + 1 < nums.length) {
+            //         nums[j] = nums[j + 1];
+            //         j++;
+            //     }
+            // }    
+        }
+
+          // APROACH 2     
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
